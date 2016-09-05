48c06fb1bf4e48fe0bd05e5b55ff0dc0c698cb23
Liverm0r <arturdumchev@yandex.ru>
2016-09-05 03:52:00 +0300

    ## change target version to 24 ## add permission check

--- app/src/main/java/com/livermor/face/screen/start/MainActivity.kt
+++ app/src/main/java/com/livermor/face/screen/start/MainActivity.kt
@@ -1,14 +1,18 @@
 package com.livermor.face.screen.start
 
+import android.Manifest
 import android.app.Activity
 import android.content.Intent
 import android.content.pm.ActivityInfo
+import android.content.pm.PackageManager
 import android.net.Uri
 import android.os.Bundle
 import android.provider.MediaStore
+import android.support.v4.content.ContextCompat
 import android.support.v7.app.AppCompatActivity
 import android.util.Log
 import com.livermor.face.R
+import com.livermor.face.screen.common.StoragePermissionDialogFragment
 import com.livermor.face.screen.face.FaceDotsActivity
 import kotlinx.android.synthetic.main.activity_main.*
 import java.io.File
@@ -16,7 +20,10 @@ import java.io.File
 
 class MainActivity : AppCompatActivity() {
 
-    val TAG: String = MainActivity::class.java.simpleName
+    companion object {
+        private val TAG: String = MainActivity::class.java.simpleName
+        private val CAMERA_REQUEST = 1888
+    }
 
     private val path: String
             by lazy { getExternalFilesDir(null).toString() + getString(R.string.photo_name) }
@@ -27,19 +34,30 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
 
-        Log.i(TAG, "path == " + path)
-        Log.i(TAG, "imageUri == " + imageUri)
-
         button.setOnClickListener {
 
-            val cameraIntent = Intent(MediaStore.ACTION_IMAGE_CAPTURE)
-            cameraIntent.putExtra(MediaStore.EXTRA_FULL_SCREEN, true)
-            cameraIntent.putExtra(MediaStore.EXTRA_OUTPUT, imageUri)
-            cameraIntent.putExtra(MediaStore.EXTRA_SCREEN_ORIENTATION, ActivityInfo.SCREEN_ORIENTATION_PORTRAIT)
-            startActivityForResult(cameraIntent, CAMERA_REQUEST)
+            if (isPermissionGranted()) {
+                startActivityForResult(buildIntentToCamera(), CAMERA_REQUEST)
+
+            } else {
+                StoragePermissionDialogFragment.newInstance().show(supportFragmentManager,
+                        StoragePermissionDialogFragment::class.java.simpleName)
+            }
         }
     }
 
+    private fun buildIntentToCamera(): Intent {
+        val cameraIntent = Intent(MediaStore.ACTION_IMAGE_CAPTURE)
+        cameraIntent.putExtra(MediaStore.EXTRA_FULL_SCREEN, true)
+        cameraIntent.putExtra(MediaStore.EXTRA_OUTPUT, imageUri)
+        cameraIntent.putExtra(MediaStore.EXTRA_SCREEN_ORIENTATION, ActivityInfo.SCREEN_ORIENTATION_PORTRAIT)
+        return cameraIntent
+    }
+
+    private fun isPermissionGranted(): Boolean {
+        return ContextCompat.checkSelfPermission(this,
+                Manifest.permission.WRITE_EXTERNAL_STORAGE) == PackageManager.PERMISSION_GRANTED
+    }
 
     override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
 
@@ -50,10 +68,5 @@ class MainActivity : AppCompatActivity() {
         } else if (resultCode == Activity.RESULT_CANCELED) {
 //            MyToast.show("вы вышли из камеры")
         }
-    }
-
-    companion object {
-
-        private val CAMERA_REQUEST = 1888
     }
 }
